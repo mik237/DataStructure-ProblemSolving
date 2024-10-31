be3128c924dfb44d3baa3a9d36ada929b91cfb16
muhammadibrahim <mik237@gmail.com>
2024-10-31 14:07:54 +0400

    checking if two strings are Anagrams

--- app/src/main/java/me/ibrahim/datastructure_problemsolving/algorithms/Algorithms.kt
+++ app/src/main/java/me/ibrahim/datastructure_problemsolving/algorithms/Algorithms.kt
@@ -2,10 +2,14 @@ package me.ibrahim.datastructure_problemsolving.algorithms
 
 
 fun main() {
-    Algorithms.findFirstNonRepeatedChar("goubemhkhgbvdkhoiubdkhkvfkhweiugdkjvhdskfhskbksfdwoeu")
+//    Algorithms.findFirstNonRepeatedChar("goubemhkhgbvdkhoiubdkhkvfkhweiugdkjvhdskfhskbksfdwoeu")
+
+    Algorithms.areAnagrams("listen", "silent")
 }
 
 object Algorithms {
+
+    //Write a function that finds the first non-repeated character in a string.
     fun findFirstNonRepeatedChar(input: String) {
         val charFrequency = mutableMapOf<Char, Int>()
 
@@ -20,5 +24,22 @@ object Algorithms {
 
         println("NonRepeating: $firstNonRepeating")
         println("Repeating: $firstRepeating")
+    }
+
+
+    //Write a function to check if two strings are anagrams.
+    //if both the strings contain same characters, regardless of order.
+    //.e.g. silent == listen ==> true
+    fun areAnagrams(str1: String, str2: String) {
+        println(str1)
+        println("Sorted-1: ${str1.toCharArray().sorted()}")
+        println(str2)
+        println("Sorted-2: ${str2.toCharArray().sorted()}")
+
+//        referencial equality which will return false (comparing references of two objects)
+//        val anagram = str1.toCharArray().sorted() === str2.toCharArray().sorted()
+
+        val anagram = str1.toCharArray().sorted() == str2.toCharArray().sorted()
+        println("Anagram: $anagram")
     }
 }
